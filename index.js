8769ac65a30bed361365ba937a9e3516f333fea9
abhijit sinha <abhijit.sinha@imaginea.com>
2020-10-15 17:48:54 +0530

    add express server

---
+++ index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.status(200).sendFile(path.join(__dirname, 'public/html/index.html')));
+
+app.listen(4000, '0.0.0.0', () => console.log('Sudoku Helper is running'));
